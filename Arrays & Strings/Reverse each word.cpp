ce8cbf865430b2f668442b4e44cebd1784ecdf6f
Pankaj Gupta <>
2021-02-20 10:01:20 +0530

    Create Reverse each word.cpp
    Create Reverse each word

---
+++ Arrays & Strings/Reverse each word.cpp
@@ -0,0 +1,52 @@
+/*Code : Reverse each word
+Aadil has been provided with a sentence in the form of a string as a function parameter. The task is to implement a function so as to print the sentence such that each word in the sentence is reversed.
+Example:
+Input Sentence: "Hello, I am Aadil!"
+The expected output will print, ",olleH I ma !lidaA".
+Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. The input string represents the sentence given to Aadil.
+Output Format:
+The only line of output prints the sentence(string) such that each word in the sentence is reversed. 
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+Welcome to Coding Ninjas
+Sample Output 1:
+emocleW ot gnidoC sajniN
+Sample Input 2:
+Always indent your code
+Sample Output 2:
+syawlA tnedni ruoy edoc
+*/
+
+// SOLUTION:
+void reverseEachWord(char input[]) {
+    // Write your code here
+    int prev = -1;
+    for(int i = 0; i <= strlen(input); i++){
+        if(input[i] == ' ' || input[i] == '\0'){
+            reverse(input + prev + 1, input + i);
+            prev = i;
+        }
+    }
+}
+
+// Main Code:
+/*
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#include "solution.h"
+
+int main() {
+    int size = 1e6;
+    char str[size];
+    cin.getline(str, size);
+    reverseEachWord(str);
+    cout << str;
+}
+*/
