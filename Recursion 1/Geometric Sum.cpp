e3c1806f53349d08457683eadec555d3787a5a16
Pankaj Gupta <>
2021-02-21 09:40:23 +0530

    Create Geometric Sum.cpp

---
+++ Recursion 1/Geometric Sum.cpp
@@ -0,0 +1,43 @@
+/* Code: Geometric Sum
+Given k, find the geometric sum i.e.
+1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k) 
+using recursion.
+Input format :
+Integer k
+Output format :
+Geometric sum (upto 5 decimal places)
+Constraints :
+0 <= k <= 1000
+Sample Input 1 :
+3
+Sample Output 1 :
+1.87500
+Sample Input 2 :
+4
+Sample Output 2 :
+1.93750
+*/
+
+// Solution:
+double geometricSum(int k){
+  	if(k == 0){
+     	return 1;   
+  	}
+    int ans = pow(2, k);
+    return geometricSum(k - 1) + ((double) 1 / ans);
+}
+
+// Main Code:
+/*
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    cout << fixed << setprecision(5);
+    cout << geometricSum(k) << endl;   
+}
+*/
