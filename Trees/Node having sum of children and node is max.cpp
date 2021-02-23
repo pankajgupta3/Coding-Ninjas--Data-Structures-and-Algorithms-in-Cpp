b9cfdd03bdef7e3093829a46bbb2fa71a71a8a9e
Pankaj Gupta <pankajguptakota@gmail.com>
2021-02-24 01:29:18 +0530

    Create Node having sum of children and node is max.cpp

---
+++ Trees/Node having sum of children and node is max.cpp
@@ -0,0 +1,206 @@
+/* Code: Node having sum of children and node is max
+Given a tree, find and return the node for which sum of data of all children and the node itself is maximum. In the sum, data of node itself and data of immediate children is to be taken.
+Input format :
+
+Line 1 : Elements in level order form separated by space (as per done in class). Order is -
+
+Root_data, n (No_Of_Child_Of_Root), n children, and so on for every element
+
+Output format : Node with maximum sum.
+
+Sample Input 1 :
+5 3 1 2 3 1 15 2 4 5 1 6 0 0 0 0
+Sample Output 1 :
+1
+*/
+
+// Solution:
+#include<climits>
+template <typename T>
+class MaxNodePair{
+    public:
+    TreeNode<T> *node;
+    int sum;
+};
+MaxNodePair<int>* maxSumNodeHelper(TreeNode<int>* root){
+    if(root == NULL){
+        MaxNodePair<int>* pair = new MaxNodePair<int>();
+        pair->node = NULL;
+        pair->sum = INT_MIN;
+        return pair;
+    }
+    int sum = root -> data;
+    for(int i = 0; i < root -> numChildren(); i++){
+        sum += root -> getChild(i)->data;
+    }
+    MaxNodePair<int>* ans = new MaxNodePair<int>();
+    ans -> node = root;
+    ans -> sum = sum;
+    for(int i = 0; i < root -> numChildren(); i++){
+        MaxNodePair<int>* childAns = maxSumNodeHelper(root -> getChild(i));
+        if(childAns -> sum > ans->sum){
+            ans = childAns;
+        }
+    }
+    return ans;
+}
+
+TreeNode<int>* maxSumNode(TreeNode<int> *root){
+    // Write your code here
+	return maxSumNodeHelper(root) -> node;
+}
+
+// Main Code:
+/*
+template <typename T>
+class TreeNode {
+	TreeNode<T>** children;
+	int childCount;
+    	int maxChildCount;
+	public:
+		T data;
+
+		TreeNode(T data) {
+			this->data = data;
+			childCount = 0;
+			children = new TreeNode<T>*[10];
+		   	maxChildCount = 10;
+        }
+
+		int numChildren() {
+			return childCount;
+		}
+
+		void addChild(TreeNode<T>* child) {
+			children[childCount] = child;
+			childCount++;
+            if(childCount == maxChildCount) {
+	            TreeNode<T>** childrenNew = new TreeNode<T>*[2*maxChildCount];
+        	    for(int i = 0; i < maxChildCount; i++) {
+	                childrenNew[i] = children[i];
+        	     }
+	        	 maxChildCount *= 2;
+        	        children = childrenNew;
+	        }
+		}
+
+		TreeNode<T>* getChild(int index) {
+			return children[index];
+		}
+
+		void setChild(int index, TreeNode<T>* child) {
+			children[index] = child;
+		}
+};
+
+template <typename T>
+class Queue {
+	T* data;
+	int capacity;
+	int nextIndex;
+	int firstIndex;
+	int length;
+
+	public:
+		Queue() {
+			capacity = 10;
+			data = new T[capacity];
+			length = 0;
+			nextIndex = 0;
+			firstIndex = -1;
+		}
+
+		int size() {
+			return length;
+		}
+
+		bool isEmpty() {
+			return length == 0;
+		}
+
+		T front() {
+			if (length == 0) {
+				return 0;
+			}
+			return data[firstIndex];
+		}
+
+		T dequeue() {
+			if (length == 0) {
+				return 0;
+			}
+
+			T output = data[firstIndex];
+			length--;
+			firstIndex = (firstIndex + 1)%capacity;
+			if (length == 0) {
+				firstIndex = -1;
+				nextIndex = 0;
+			}
+			return output;
+		}
+
+		void enqueue(T element) {
+			if (length == capacity) {
+				T* temp = data;
+				capacity = 2* capacity;
+				data = new T[capacity];
+				int k = 0;
+				for (int i = firstIndex; i < length; i++) {
+					data[k] = temp[i];
+					k++;
+				}
+				for (int i = 0; i < firstIndex; i++) {
+					data[k] = temp[i];
+					k++;
+				}
+				delete [] temp;
+				firstIndex = 0;
+				nextIndex = length;
+			}
+			data[nextIndex] = element;
+			length++;
+			nextIndex = (nextIndex + 1) % capacity;
+			if (firstIndex == -1) {
+				firstIndex = 0;
+			}
+		}
+
+		~Queue() {
+			delete [] data;
+		}
+
+
+};
+
+#include <iostream>
+using namespace std;
+TreeNode<int>* takeInputLevelWise() {
+	Queue<TreeNode<int>*> q;
+	int rootData;
+
+	cin >> rootData;
+	TreeNode<int>* root = new TreeNode<int>(rootData);
+	q.enqueue(root);
+	while (!q.isEmpty()) {
+		TreeNode<int>* frontNode = q.dequeue();
+
+		int numChildren;
+		cin >> numChildren;
+		for (int i = 0; i < numChildren; i++) {
+
+			int childData;
+			cin >> childData;
+			TreeNode<int>* childNode = new TreeNode<int>(childData);
+			q.enqueue(childNode);
+			frontNode->addChild(childNode);
+		}
+	}
+	return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    cout << maxSumNode(root) -> data << endl;
+}
+*/
