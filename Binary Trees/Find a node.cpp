cc96a29951a465fb476f56330dc773b6accd592d
Pankaj Gupta <pankajguptakota@gmail.com>
2021-02-26 17:46:08 +0530

    Create Find a node.cpp

---
+++ Binary Trees/Find a node.cpp
@@ -0,0 +1,102 @@
+/* Problem: Find a node
+For a given Binary Tree of type integer and a number X, find whether a node exists in the tree with data X or not.
+ Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The only line of output prints 'true' or 'false'.
+Note:
+You are not required to print anything explicitly. It has already been taken care of.
+Constraints:
+1 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+7
+Sample Output 1:
+true
+Sample Input 2:
+2 3 4 -1 -1 -1 -1
+10
+Sample Output 2:
+false
+*/
+
+// Solution:
+bool isNodePresent(BinaryTreeNode<int>* root, int x) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+    
+    if(root == NULL){
+        return false;
+    }  
+    
+    if(root -> data == x){
+        return true;
+    }
+    
+    bool res1 = isNodePresent(root -> left, x);
+    bool res2 = isNodePresent(root -> right, x);
+    return res1 || res2;
+}
+
+// Main Code:
+/*
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+using namespace std;
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int> *root = takeInput();
+    int x;
+    cin >> x;
+    cout << ((isNodePresent(root, x)) ? "true" : "false");
+}
+*/
